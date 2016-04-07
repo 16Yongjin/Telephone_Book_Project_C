8adcfcd5e773fea70220d790b41cb79a3f903003
YONGJIN JO <whrhqn@naver.com>
2016-04-07 15:22:32 +0900

    Initial Commit

---
+++ project 2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  project 2
+//
+//  Created by JoYongJin on 2016. 4. 7..
+//  Copyright © 2016년 JoYongJin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
